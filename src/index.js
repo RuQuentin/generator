4fe1529fb0a891a4aa073b7676a049fc087a036c
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-01-16 15:44:37 +0300

    completely finish the project, change project logic to using callback function

--- src/index.js
+++ src/index.js
@@ -1,101 +1,113 @@
 // 'use strict'
 
 function runner(iter) {
-  function isPromise(value) {
-    return value instanceof Promise;
-  }
+  return new Promise (resolve => {
+    function isPromise(value) {
+      return value instanceof Promise;
+    }
+  
+    function isFunction(value) {
+      return typeof value === 'function';
+    }
 
-  function isFunction(value) {
-    return typeof value === 'function';
-  }
+    const outputArr = [];
+    let realValue = null;
+    let result = iter.next();
+    let done = false;
 
-  let promiseChain = Promise.resolve();
-
-  const outputArr = [];
-  // let result = null;
-  let realValue = null;
-  // let done = false;
-
-  function addThen(realValue) {
-    promiseChain = new Promise( resolve => {
-      // console.log(realValue)
-      result = iter.next(realValue);
+    function iterate(result) {
       const { value } = result;
-      // const { done } = result;
-            
+      done = result.done;
+
       if (value === undefined) {
-        // console.log('value = undefined')
+        resolve(outputArr)
+        
       } else if (!isPromise(value) && !isFunction(value)) {
-        // console.log('value')
         realValue = value;
       } else if (isPromise(value)) {
-        // console.log('promise')
 
-        resolve(value.then(realValue => {
-          return {result, realValue};
-        }
-        ))
+        value.then(data => {
+        result['value'] = data;
+        return result;
+        })
+        .then(result => iterate(result))
 
       } else if (isFunction(value)) {
-        // console.log('function')
         realValue = value();
       }
 
-      resolve({result, realValue});
-    })
+      if (realValue !== null) {
+        outputArr.push(realValue);
+  
+        if (done === true) {
+          resolve(outputArr)
+        }
+  
+        result = iter.next(realValue);
+        realValue = null;
+  
+        iterate(result);
+      } 
+    };
 
-    .then( data => {
-      outputArr.push(data['realValue']);
-
-      if (data['result'].done) {
-        return outputArr;
-      }
-
-      return data['realValue'];
-    })
-
-    .then ( data => {
-      if (data === outputArr) {
-        return data;
-      }
-
-      addThen(data)
-    })
-  };
-
-  addThen(realValue);
-
-
-  return promiseChain.then(data => data);
+    iterate(result);
+  })
 }
 
 
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const val = 20;
+
+// const prom = new Promise(res => {
+//   setTimeout(res, 1000, 10)
+// });
+
+
+// function *gen() {
+//     const a = yield () => sum(1, 2);
+//     const b = yield prom;
+//     const c = yield val;
+//     const d = yield prom;
+//     const e = yield val;
+//     const f = yield () => sum(2, 5);
+//     console.log(a, b, c, d, e, f)
+// }
+
+// const iter = gen();
+
+// // console.log(iter.next());
+// // console.log(iter.next());
+// // console.log(iter.next());
+// // console.log(iter.next());
+
+// runner(gen()).then(console.log)
+
+function sum() {
+  console.log(1);
+  return [].reduce.call(arguments, (acc, el) => acc+=el);
 }
 
-const val = 20;
+const prom = x => new Promise(res => {
+  console.log(2);
+  setTimeout(res,2000,x);
+})
 
-const prom = new Promise(res => {
-  setTimeout(res, 1000, 10)
-});
+function pow() {
+  console.log(3);
+  return [].reduce.call(arguments, (acc, el) => acc*=el);
+}
 
+const arr = [1,2,3,4]
 
 function *gen() {
-    const a = yield () => sum(1, 2);
-    const b = yield prom;
-    const c = yield val;
-    const d = yield prom;
-    const e = yield val;
-    const f = yield () => sum(2, 5);
-    console.log(a, b, c, d, e, f)
+  const a = yield sum.bind(null, ...arr);
+  const b = yield prom(a);
+  const c = yield pow.bind(null, ...arr);
+  const d = yield arr;
+  yield a + b + c + d;
 }
 
-const iter = gen();
-
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-
-runner(gen()).then(console.log)
+runner(gen()).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"))
